bc2a4560948a41b5587f1a8bea1a9de82b31bc8e
Sergey-Ivanovich <ericnoata@gmail.com>
2024-04-17 09:21:40 +0100

    began adding add option

--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 function handleSubmit(event) {
   event.preventDefault();
+  console.log(event.target[0].value);
 }
 
 let listItem = document.querySelectorAll("li");
 let entryBoxElement = document.querySelector("#entryBox");
 let entryFormElement = document.querySelector("#entryForm");
+let listContainerElement = document.querySelector(".list-container");
+let activeItems = listItem.length;
+let inactiveItems = 0;
 
-entrySubmitElement.addEventListener("submit", handleSubmit);
+entryFormElement.addEventListener("submit", handleSubmit);
 
-listItem.forEach(function (item, index) {
+listItem.forEach(function (item) {
   item.addEventListener("click", crossItem);
 
   function crossItem() {
-    item.classList.toggle("cross");
+    item.classList.add("cross");
 
     setTimeout(eliminateItem, 1000);
   }
 
   function eliminateItem() {
+    inactiveItems += 1;
     item.classList.add("eliminate");
+    if (activeItems - inactiveItems === 0) {
+      listContainerElement.innerHTML = `<span class="text-black-50 text-center"> <h3 class="mb-3"> To-Do List is Empty </h3></span> `;
+    }
   }
 });
